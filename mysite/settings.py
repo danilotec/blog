001f68713e333372de7def21e13d24ffb5f00f55
danilotec <danilo0123499@gmail.com>
2025-09-22 20:01:56 -0300

    um host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0qgk^e$-@fudix(a2qj0gl0wv3v17832xk!#4_kz-t@j59#pwx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', 'blog.omnisistems.com.br']
+ALLOWED_HOSTS = ['blog.omnisistems.com.br']
 
 
 # Application definition
